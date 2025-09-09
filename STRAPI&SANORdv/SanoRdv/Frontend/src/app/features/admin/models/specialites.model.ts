527b0d85c872c755634006c1cbe96f4dc3fda37a
KAGAMBEGA Boukary <104064641+SoftWareEngeneerDev@users.noreply.github.com>
2025-09-09 09:22:20 +0000

    Delete STRAPI&SANORdv/SanoRdv/Frontend directory
    Suppression

--- STRAPI&SANORdv/SanoRdv/Frontend/src/app/features/admin/models/specialites.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Specialite {
-  _id?: string; 
-  nom: string;
-  description: string;
-  nombreMedecins?: number;
-}
--- STRAPI&SANORdv/SanoRdv/Frontend/src/app/shared/models/medecin.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Medecin {
-  IDmedecin?: string;
-  medecinId?: string;
-}
